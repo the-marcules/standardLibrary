60ae94c9a8a2396b88157989dc18bbdd7b91e147
marcules <markus2.fischer@audi.de>
2025-09-02 21:49:09 +0200

    full wp setup

--- wordpress/local-dev-env/docker/webserver/entrypoint.sh
+++ wordpress/local-dev-env/docker/webserver/entrypoint.sh
@@ -38,10 +38,24 @@ wp option update wopro_paypal_client_secret_sandbox 888 --allow-root
 wp option update wopro_paypal_sandbox_toggle 1 --allow-root
 wp option update wopro_shipping_price 888 --allow-root
 
+
+HEADER_MENU_NAME="Haupt"
+HEADER_MENU_LOCATION="header-menu"
+
+FOOTER_MENU_NAME="Footer"
+FOOTER_MENU_LOCATION="footer-menu"
+
+wp menu create "$HEADER_MENU_NAME" --allow-root
+wp menu location assign "$HEADER_MENU_NAME" $HEADER_MENU_LOCATION --allow-root
+
+wp menu create "$FOOTER_MENU_NAME" --allow-root
+wp menu location assign "$FOOTER_MENU_NAME" $FOOTER_MENU_LOCATION --allow-root
+
 # Seite "Shop" erstellen und ID speichern
 SHOP_ID=$(wp post create --post_type=page --post_title="Shop" --post_content="" --post_status=publish --porcelain --allow-root)
 # Template setzen
 wp post meta update $SHOP_ID _wp_page_template all-wopro_product.php --allow-root
+wp menu item add-post "$HEADER_MENU_NAME" $SHOP_ID --allow-root
 
 # Seite "Deine Bestellung"
 ORDER_ID=$(wp post create --post_type=page --post_title="Deine Bestellung" --post_content="" --post_status=publish --porcelain --allow-root)
@@ -50,6 +64,36 @@ wp post meta update $ORDER_ID _wp_page_template wopro-order-confirm.php --allow-
 # Seite "Warenkorb"
 CART_ID=$(wp post create --post_type=page --post_title="Warenkorb" --post_content="" --post_status=publish --porcelain --allow-root)
 wp post meta update $CART_ID _wp_page_template wopro-cart.php --allow-root
+
+# Seite "Kontakt"
+CART_ID=$(wp post create --post_type=page --post_title="Kontakt" --post_content="" --post_status=publish --porcelain --allow-root)
+wp post meta update $CART_ID _wp_page_template contact-form.php --allow-root
+wp menu item add-post "$HEADER_MENU_NAME" $CART_ID --allow-root
+
+# Seite "Individuelle Fertigung"
+INDIVIDUAL_ID=$(wp post create --post_type=page --post_title="Individuelle Fertigung" --post_content="" --post_status=publish --porcelain --allow-root)
+wp menu item add-post "$HEADER_MENU_NAME" $INDIVIDUAL_ID --allow-root
+
+# Seite "Individuelle Fertigung"
+ABOUT_ID=$(wp post create --post_type=page --post_title="Über mich" --post_content="" --post_status=publish --porcelain --allow-root)
+wp menu item add-post "$HEADER_MENU_NAME" $ABOUT_ID --allow-root
+
+
+# Seite "AGB"
+AGB_ID=$(wp post create --post_type=page --post_title="AGB" --post_content="" --post_status=publish --porcelain --allow-root)
+wp menu item add-post "$FOOTER_MENU_NAME" $AGB_ID --allow-root
+
+# Seite "Datenschutz"
+DATENSCHUTZ_ID=$(wp post create --post_type=page --post_title="Datenschutz" --post_content="" --post_status=publish --porcelain --allow-root)
+wp menu item add-post "$FOOTER_MENU_NAME" $DATENSCHUTZ_ID --allow-root
+
+# Seite "Impressum"
+IMPRESSUM_ID=$(wp post create --post_type=page --post_title="Impressum" --post_content="" --post_status=publish --porcelain --allow-root)
+wp menu item add-post "$FOOTER_MENU_NAME" $IMPRESSUM_ID --allow-root
+
+# Testprodukt anlegen
+TEST_PRODUCT=$(wp post create --post_type=wopro_product --post_title="Testprodukt" --post_content="Dies ist ein Testprodukt." --post_status=publish --porcelain --allow-root)
+wp post meta update $TEST_PRODUCT price 25.99 --allow-root
 
 
 echo "Wordpress erfolgreich eingerichtet: 🔗 '$WORDPRESS_URL'"
