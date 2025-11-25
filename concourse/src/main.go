74819d89152a5ce17fc8baf62fc9e72f957dc615
marcules <markus2.fischer@audi.de>
2025-11-25 14:34:04 +0100

    Refactor code structure for improved readability and maintainability

--- concourse/src/main.go
+++ concourse/src/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+	if len(args) == 2 {
+		a, _ := strconv.Atoi(args[0])
+		b, _ := strconv.Atoi(args[1])
+		sum := add(a, b)
+		fmt.Printf("Sum of %d and %d is %d\n", a, b, sum)
+	} else {
+		fmt.Println("Please provide two arguments.")
+		os.Exit(1)
+	}
+
 }
 
 func add(a, b int) int {
