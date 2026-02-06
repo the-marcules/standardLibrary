18d12e08e9b427910a2bb354f7a244f31f39de73
Markus Fischer <markus2.fischer@audi.de>
2026-02-06 14:58:25 +0100

    refactor upload handler

--- go/httptesting/routes/upload/upload.go
+++ go/httptesting/routes/upload/upload.go
@@ -2,6 +2,8 @@ package upload
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
@@ -19,18 +21,52 @@ type FileMeta struct {
 
 func FileUploadHandler(w http.ResponseWriter, r *http.Request) {
 
+	fileMeta, statusCode, err := createFileFromMultipart(r)
+	if err != nil {
+		slog.Error("could not create file from multipart form", "error", err.Error())
+		w.WriteHeader(statusCode)
+		return
+	}
+
+	statusCode, err = checkFileHash(fileMeta)
+	if err != nil {
+		slog.Error(err.Error())
+		w.WriteHeader(statusCode)
+		return
+	}
+
+	w.WriteHeader(statusCode)
+}
+
+func checkFileHash(fileMeta *FileMeta) (int, error) {
+	fileHash, err := util.HashFile(tmpDir + "/" + fileMeta.Filename)
+	if err != nil {
+		err = errors.New("could not hash file. error:" + err.Error())
+		return http.StatusInternalServerError, err
+
+	}
+
+	if fileMeta.Hash != fileHash {
+		_ = os.Remove(tmpDir + "/" + fileMeta.Filename)
+		err = errors.New("file hashes do not match")
+		return http.StatusBadRequest, err
+
+	}
+
+	return http.StatusOK, nil
+}
+
+func createFileFromMultipart(r *http.Request) (*FileMeta, int, error) {
 	err := r.ParseMultipartForm(1000 * 1000 * 400)
 	if err != nil {
-		slog.Error("could not parse multipart form", "error", err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		err = errors.New(fmt.Sprintf("could not parse multipart form. %s: %s", "error", err.Error()))
+		return nil, http.StatusBadRequest, err
 	}
 
 	file, _, err := r.FormFile("file")
 	if err != nil {
-		slog.Error("could not get file from form", "error", err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		err = errors.New(fmt.Sprintf("could not get file from form. %s: %s", "error", err.Error()))
+		return nil, http.StatusBadRequest, err
 	}
 	defer file.Close()
 
@@ -39,59 +75,35 @@ func FileUploadHandler(w http.ResponseWriter, r *http.Request) {
 	var fileMeta FileMeta
 	err = json.Unmarshal([]byte(wert), &fileMeta)
 	if err != nil {
-		slog.Error("could not parse file meta data", "error", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		err = errors.New(fmt.Sprintf("could not parse file meta data. %s: %s", "error", err.Error()))
+		return nil, http.StatusInternalServerError, err
 	}
 
 	err = os.MkdirAll(tmpDir, 0o755)
 	if err != nil {
-		slog.Error("could not create temp dir", "error", err.Error(), "tmpDir", tmpDir)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		err = errors.New(fmt.Sprintf("could not create temp dir. %s: %s", "error", err.Error()))
+		return nil, http.StatusInternalServerError, err
 	}
 
 	tmpFile, err := os.CreateTemp(tmpDir, "uploaded-*")
 	defer tmpFile.Close()
 	if err != nil {
-		slog.Error("could not create temp file", "error", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		err = errors.New(fmt.Sprintf("could not create temp file. %s: %s", "error", err.Error()))
+		return nil, http.StatusInternalServerError, err
 	}
 
 	_, err = io.Copy(tmpFile, file)
 	defer r.Body.Close()
 	if err != nil {
-		slog.Error("could not write body to tmp file", "error", err.Error(), "tmpFile", tmpDir+"/"+tmpFile.Name())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		err = errors.New(fmt.Sprintf("could not write body to tmp file. %s: %s", "error", err.Error()))
+		return nil, http.StatusInternalServerError, err
 	}
 
 	err = os.Rename(tmpFile.Name(), tmpDir+"/"+fileMeta.Filename)
 	if err != nil {
-		slog.Error("could not rename file", "error", err.Error(), "tmpFile", tmpDir+"/"+tmpFile.Name(), "targetFile", tmpDir+"/"+fileMeta.Filename)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		err = errors.New(fmt.Sprintf("could not rename file. %s: %s", "error", err.Error()))
+		return nil, http.StatusInternalServerError, err
 	}
 
-	fileHash, err := util.HashFile(tmpDir + "/" + fileMeta.Filename)
-	if err != nil {
-		slog.Error("could not hash file", "error", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if fileMeta.Hash != fileHash {
-		_ = os.Remove(tmpDir + "/" + fileMeta.Filename)
-
-		slog.Error("file hashes do not match")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-}
-
-func createFileFromMultipart(r *http.Request) (FileMeta, error) {
-
+	return &fileMeta, http.StatusOK, nil
 }
